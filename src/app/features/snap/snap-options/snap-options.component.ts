a4d153238e2d6d2e3fdbcaadc7f680ca82a905fb
EuCaue <caueadriano.castro@gmail.com>
2024-10-01 21:33:27 -0300

    feat(snap-options): use snap-service to set the snapOptions

--- src/app/features/snap/snap-options/snap-options.component.ts
+++ src/app/features/snap/snap-options/snap-options.component.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { DialogModule } from 'primeng/dialog';
 import { InputSwitchModule } from 'primeng/inputswitch';
 import { InputTextModule } from 'primeng/inputtext';
+import { SnapService } from '../services/snap.service';
+import { SnapOptions } from '../types/types';
 
 @Component({
   selector: 'app-snap-options',
@@ -19,29 +21,39 @@ import { InputTextModule } from 'primeng/inputtext';
   styleUrl: './snap-options.component.css',
 })
 export class SnapOptionsComponent {
-  @Input() showPopup: boolean = false;
+  @Input() showPopup: boolean = Boolean();
   @Output() showPopupChange = new EventEmitter<boolean>();
-  viewport: string = '1920x1080';
-  captureFullPage: boolean = false;
   isViewPortInvalid: boolean = false;
+  snapOptions: SnapOptions;
 
-  updateShowPopup(state: boolean){
-    this.showPopup = state;
-    this.showPopupChange.emit(this.showPopup)
+  constructor(private snapService: SnapService) {
+    this.snapOptions = this.snapService.getSnapOptions();
   }
 
+  updateShowPopup(state: boolean) {
+    this.showPopup = state;
+    this.showPopupChange.emit(this.showPopup);
+  }
+
+  updateCaptureFullPage(captureFullPage: boolean) {
+    this.snapService.setSnapOptions({ captureFullPage });
+  }
+
+  updateViewport(viewport: string) {
+    this.snapService.setSnapOptions({ viewport });
+  }
 
   checkViewPort(viewport: string) {
-    // @ts-ignore
     const [width, height] = viewport.split('x').map(Number);
     const regex = /^\d+x\d+$/;
 
     if (!regex.test(viewport) || width <= 0 || height <= 0) {
       this.isViewPortInvalid = true;
-      return false;
+      return true;
     }
 
     this.isViewPortInvalid = false;
-    return true;
+    this.updateViewport(viewport);
+    return false;
   }
 }
