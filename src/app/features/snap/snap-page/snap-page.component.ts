174ca717dcd39fd2851c99ad20ee62abbb4b326a
EuCaue <caueadriano.castro@gmail.com>
2024-10-01 21:54:10 -0300

    refactor(snap-page): remove unused code

--- src/app/features/snap/snap-page/snap-page.component.ts
+++ src/app/features/snap/snap-page/snap-page.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { SnapService } from '../../../core/services/snap.service';
 import { SnapFormComponent } from '../snap-form/snap-form.component';
 import { SnapOptionsComponent } from '../snap-options/snap-options.component';
 import { SnapPreviewComponent } from '../snap-preview/snap-preview.component';
@@ -8,21 +7,6 @@ import { SnapPreviewComponent } from '../snap-preview/snap-preview.component';
   selector: 'app-snap-page',
   templateUrl: './snap-page.component.html',
   standalone: true,
-  imports: [SnapFormComponent, SnapPreviewComponent, SnapOptionsComponent] // Importar os componentes aqui
+  imports: [SnapFormComponent, SnapPreviewComponent, SnapOptionsComponent],
 })
-export class SnapPageComponent {
-  showSnapedImage: boolean = false;
-  imageUrl: string = '';
-  captureFullPage: boolean = false;
-  viewport: string = '1920x1080';
-
-  constructor(private snapService: SnapService) {}
-
-  handleSnapWebsite(url: string): void {
-    this.snapService.getSnap(url).subscribe(response => {
-      this.showSnapedImage = true;
-      console.log(response)
-      this.imageUrl = 'https://some-image-url.com';
-    });
-  }
-}
+export class SnapPageComponent {}
