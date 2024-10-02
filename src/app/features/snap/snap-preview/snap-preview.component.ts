7952f77caa8c209fb5d3d5c68b1980675757e7ea
EuCaue <caueadriano.castro@gmail.com>
2024-10-01 21:58:16 -0300

    feat(snap-preview): implement download image function
    - Implemented download image logic
    - Improved the HTML layout

--- src/app/features/snap/snap-preview/snap-preview.component.ts
+++ src/app/features/snap/snap-preview/snap-preview.component.ts
@@ -5,31 +5,55 @@ import { ImageModule } from 'primeng/image';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { SplitButtonModule } from 'primeng/splitbutton';
 import { Observable, Subscription } from 'rxjs';
-import { SnapService } from '../../../core/services/snap.service';
+import { SnapService } from '../services/snap.service';
+import { SnapData } from '../types/types';
 
-type Data = {
-  snapUrl: string;
-  showSnappedImage: boolean;
-};
 @Component({
   selector: 'app-snap-preview',
   standalone: true,
-  imports: [CommonModule, ImageModule, CardModule, SplitButtonModule, ProgressSpinnerModule],
+  imports: [
+    CommonModule,
+    ImageModule,
+    CardModule,
+    SplitButtonModule,
+    ProgressSpinnerModule,
+  ],
   templateUrl: './snap-preview.component.html',
   styleUrl: './snap-preview.component.css',
 })
 export class SnapPreviewComponent {
-  data: Data = { snapUrl: '', showSnappedImage: false };
+  snapData: SnapData = { snapUrl: '', showSnappedImage: false };
   subscription: Subscription;
-  loading$: Observable<boolean>;
+  loadingPreview$: Observable<boolean>;
+  loadingDownload: boolean = false;
   showSnappedImage: boolean = false;
   constructor(private snapService: SnapService) {
-    this.loading$ = this.snapService.isLoading$;
+    this.loadingPreview$ = this.snapService.isLoading$;
     this.subscription = this.snapService.snapUrlPreview$.subscribe((data) => {
-      this.data = data;
+      this.snapData = data;
     });
   }
   ngOnDestroy() {
     this.subscription.unsubscribe();
+  }
+
+  async downloadImage() {
+    this.loadingDownload = true;
+    const imageUrl = this.snapData.snapUrl;
+    const fileName = `snapped-image-${new Date().toLocaleTimeString()}.jpg`;
+    try {
+      const response = await fetch(imageUrl);
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = fileName;
+      link.click();
+      window.URL.revokeObjectURL(url);
+    } catch (err) {
+      console.error('Error', err);
+    } finally {
+      this.loadingDownload = false;
+    }
   }
 }
